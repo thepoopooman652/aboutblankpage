13545812c6f02f2e6d6ad954afae5144ac255199
proishnoob7910 <64977705+thepoopooman652@users.noreply.github.com>
2025-05-23 09:49:27 -0400

    Update script.js
    Temporaily disabled the redirect to 403 page

--- script.js
+++ script.js
@@ -88,6 +88,7 @@ function checkRedirectSource() {
   // Check if the referrer is "thepoopooman652.github.io/index.html"
   if (referrer !== "thepoopooman652.github.io/index.html") {
     // If not redirected from the specified URL, redirect to the error page
-    window.location.href = "http://error-pages.aidanwatters.kesug.com/403.html";
+    // window.location.href = "http://error-pages.aidanwatters.kesug.com/403.html";
+    console.error("I dont fucking know why, but shit didnt work man");
   };
 };
